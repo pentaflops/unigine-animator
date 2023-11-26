8900d675965b467ea21b6b9d762cf00822b48a86
pentaflops <kras.russian@gmail.com>
2023-11-26 03:20:14 +0300

    Add example usage

---
+++ ExamplePlayerAnimatorComponent.cs
@@ -0,0 +1,31 @@
+﻿using PeAnim;
+using PeAnim.States;
+using Unigine;
+
+namespace UnigineApp.data.csharp_template.FP_controller.components;
+
+public class ExamplePlayerAnimatorComponent : Component
+{
+	public AnimationBlend2dState movementAnimation;
+	public AnimationState startJumpAnimation;
+	public AnimationState inAirAnimation;
+	public AnimationState endJumpAnimation;
+
+	private Animator animator;
+
+	private void Init()
+	{
+		animator = new Animator(node, new IAnimationState[]
+		{
+			movementAnimation,
+			startJumpAnimation,
+			inAirAnimation,
+			endJumpAnimation,
+		});
+	}
+
+	private void Update()
+	{
+		animator.Update();
+	}
+}
